6096eb3efe4132fe909e55af779b3e79e2242006
Rob-Bot-Bob <fernando.webb42@gmail.com>
2021-07-05 17:45:04 -0500

    golang y python

---
+++ LinkedinCursos/Golang/examples.go
@@ -0,0 +1,13 @@
+//examples of golang
+
+import{
+    "fmt"
+}
+
+func main(){
+    x := 10
+    
+    if > 5{
+        fmt.Println("x is big")
+    }
+}
--- LinkedinCursos/Selenium/webBrowsing.py
+++ LinkedinCursos/Selenium/webBrowsing.py
@@ -1,7 +1,29 @@
+#there is an issue if you get the driver to run in windows whiel useing wsl it will not work, you have to use powershell I couldn't figure why, the path was set but still didn't work
+from types import coroutine
 from selenium import webdriver
-driver = webdriver.Chrome()
+# from selenium.webdriver.chrome.options import Options
+# chrome_options=Options()
+
+# chrome_options.add_argument('--headless')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--no-gpu')
+# chrome_options.add_argument('--disable-setuid-sandbox')
+# chrome_options.add_argument('--single-process')
+# chrome_options.add_argument('--windo-size=1920,1080')
+
+# chrome_options.binary_location = "/usr/bin/chrome-linux/chrom"
+
+driver = webdriver.Chrome(executable_path="C:\\Program Files (x86)\\chromedriver.exe")#powershell funciona
+#driver = webdriver.Chrome(options=chrome_options)
 driver.get('https://www.seleniumeasy.com/test/basic-first-form-demo.html')
 messageField = driver.find_element_by_xpath('//*[@id="user-message"]')
 messageField.send_keys('Hello World')
 showMessageButton = driver.find_element_by_xpath('//*[@id="get-input"]/button')
 showMessageButton.click()
+additionField1 = driver.find_element_by_xpath('//*[@id="sum1"]')
+additionField1.send_keys('10')
+additionField2 = driver.find_element_by_xpath('//*[@id="sum2"]')
+additionField2.send_keys('12')
+getTotalButton = driver.find_element_by_xpath('//*[@id="gettotal"]/button')
+getTotalButton.click()
+
